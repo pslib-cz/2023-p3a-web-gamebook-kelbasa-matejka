71d7d20548d48ee41229ff7cbd7cbb2184060a38
Pixelgon <pixelgon@pixelgon.cz>
2023-12-30 13:35:02 +0100

    casti html do externich souboru pro zlepseni citelnosti.

---
+++ Game/Pages/Combat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Game.Pages
+{
+    public class CombatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
