ce5c3dd78af2b08d6905ed96970837b8b5ff659f
Pixelgon <pixelgon@pixelgon.cz>
2024-01-12 11:18:11 +0100

    Flex je pain, asi predelam layout na blocky...

---
+++ Game/wwwroot/js/location.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let packBtn= document.getElementById("packBtn");
+    let pack = document.getElementById("pack");
+    packBtn.addEventListener("click", function () {
+        if (pack.classList.contains("pack--open")) {
+            pack.classList.remove("pack--open");
+        }
+        else {
+            pack.classList.add("pack--open");
+        }
+    });
+});
